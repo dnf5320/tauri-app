83d64e66e28446b0c8974e81f0ea45819d10b1ed
jybx <jybx@duck.com>
2023-02-27 20:38:55 +0800

    feat: ✨ init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from './components/Greet.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1>王老师最帅!</h1>
+    <Greet />
+  </div>
+</template>
+
+<style scoped>
+</style>
